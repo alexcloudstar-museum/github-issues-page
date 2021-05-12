f52ea532ca32c0fc201849b21622f4cd2a9a3bf9
alexcloudstar <alexcloudstar@gmail.com>
2021-05-12 21:53:40 +0300

    Adding Theming and wired up

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import HelloWorld from 'components/HelloWorld';
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './Theme/theme';
+import GlobalStyles from './Theme/globalStyles';
 
-const App = (): JSX.Element => <HelloWorld />;
+const App = (): JSX.Element => (
+	<ThemeProvider theme={defaultTheme}>
+		<GlobalStyles />
+		<h1>Hello world!</h1>
+	</ThemeProvider>
+);
 
 export default App;
