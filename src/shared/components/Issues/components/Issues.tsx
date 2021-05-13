fbc4a3883c2e47e7f28801989feead94e070d8a8
alexcloudstar <alexcloudstar@gmail.com>
2021-05-13 23:39:38 +0300

    Fix custom env variables issues && fetched issues into Issues Comp

--- src/shared/components/Issues/components/Issues.tsx
+++ src/shared/components/Issues/components/Issues.tsx
@@ -1,9 +1,20 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 
 import Issue from './Issue';
 import { IssuesWrapper } from '../style';
+import { useActions } from 'src/hooks/useActions';
+import { useTypedSelector } from 'src/hooks/useTypedSelector';
 
-const Issues = (): JSX.Element => {
+const Issues: FC = (): JSX.Element => {
+	const issues = useTypedSelector(({ issues }) =>
+		issues.data?.map((issue) => issue)
+	);
+	const { getIssues } = useActions();
+
+	useEffect(() => {
+		getIssues();
+	}, [getIssues]);
+
 	return (
 		<IssuesWrapper>
 			<h1>Issues</h1>
